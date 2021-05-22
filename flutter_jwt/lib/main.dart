658b7c147d4aa0441fee643700d1df8ebfbb2bf0
Julius Jacobsohn <julius.jacobsohn@udo.edu>
2021-05-22 15:45:47 +0200

    Make app stateful

--- flutter_jwt/lib/main.dart
+++ flutter_jwt/lib/main.dart
@@ -26,10 +26,14 @@ class UserState with ChangeNotifier{
   }
 }
 
-class MyApp extends StatelessWidget {
-  // Read JWT from storage (secure/local)
-  // Return JWT or empty string if not existing
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
 
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
