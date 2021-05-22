8ff4c89951c77047e618ec8ae8bdd1b23c551b03
Julius Jacobsohn <julius.jacobsohn@udo.edu>
2021-05-22 11:21:15 +0200

    Add db context and api user class

---
+++ Api/Data/ApiUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Data
+{
+    /// <summary>
+    /// Extend this class according to your needs
+    /// </summary>
+    public class ApiUser : IdentityUser
+    {
+    }
+}
--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ## Backend Steps to take
 Put a secure string into the jwt section in appsettings.json (https://www.browserling.com/tools/random-string)
+(optional) Extend Data/ApiUser.cs contents according to your needs
 
 ## Frontend Steps to take
